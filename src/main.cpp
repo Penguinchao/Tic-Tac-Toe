ca6a208a370774d8870561ffa7c0c95cc9a7bb0d
Christian Russell <penguinchao@ymail.com>
2015-05-16 22:48:45 -0700

    Changed win logic and board formatting
    Player 2 is now called 'O' instead of 'Y.'
    To prevent possible bugs, win conditions now have two simple logical
    tests instead of one conjoined one.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int board[3][3];
 bool gameActive = 1;
 int turnsTaken = 0;
-bool playerTurn = 0; // 0 is X and 1 is Y
+bool playerTurn = 0; // 0 is X and 1 is O
 bool debugEnabled = 1;
 
 void resetBoard();
@@ -14,6 +14,7 @@ void makeMove();
 int checkWinner();
 void checkWin();
 void debugOut(string output);
+char getPlayerLetter(int playerNumber);
 
 int main(){
     resetBoard();
@@ -34,50 +35,74 @@ void resetBoard(){
 
 void showBoard(){
     cout << "  0   1   2 " << endl << endl
-         << "0 " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl
+         << "0 " << getPlayerLetter(board[0][0]) << " | " << getPlayerLetter(board[0][1]) << " | " << getPlayerLetter(board[0][2]) << endl
          << "----+---+----" << endl
-         << "1 " << board[1][0] << " | " << board[1][1] << " | " << board[2][2] << endl
+         << "1 " << getPlayerLetter(board[1][0]) << " | " << getPlayerLetter(board[1][1]) << " | " << getPlayerLetter(board[2][2]) << endl
          << "----+---+----" << endl
-         << "2 " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+         << "2 " << getPlayerLetter(board[2][0]) << " | " << getPlayerLetter(board[2][1]) << " | " << getPlayerLetter(board[2][2]) << endl;
 }
 
 int checkWinner(){
-    if(board[0][0] == board[0][1] == board[0][2] !=0 ){
-        debugOut("END 1");
-        return board[0][0];
-    }else if(board[1][0] == board[1][1] == board[1][2] != 0){
-        debugOut("END 2");
-        return board[1][0];
-    }else if(board[2][0] == board[2][1] == board[2][2] != 0){
-        debugOut("END 3");
-        return board[2][0];
-    }else if(board[0][0] == board[1][0] == board[2][0] != 0){
-        debugOut("END 4");
-        return board[0][0];
-    }else if(board[0][1] == board[1][1] == board[2][1] != 0){
-        debugOut("END 5");
-        return board[0][1];
-    }else if(board[0][2] == board[1][2] == board[2][2] != 0){
-        debugOut("END 6");
-        return board[0][1];
-    }else if(board[0][0] == board[1][1] == board[2][2] != 0){
-        debugOut("END 7");
-        return board[0][0];
-    }else if(board[0][2] == board[1][1] == board[2][0] != 0){
-        debugOut("END 8");
-        return board[0][1];
-    }else{
-        return 0;
+    if( (board[0][0] == board[0][1]) && (board[0][0] == board[0][2]) ){
+        if(board[0][0] != 0){
+            debugOut("END 1");
+            return board[0][0];
+        }
     }
+    if( (board[1][0] == board[1][1]) && (board[1][0] == board[1][2]) ){
+        if(board[1][0] != 0){
+            debugOut("END 2");
+            return board[1][0];
+        }
+    }
+    if( (board[2][0] == board[2][1]) && (board[2][0] == board[2][2]) ){
+        if(board[2][0] != 0){
+            debugOut("END 3");
+            return board[2][0];
+        }
+    }
+    if( (board[0][0] == board[1][0]) && (board[0][0] == board[2][0]) ){
+        if(board[0][0] != 0){
+            debugOut("END 4");
+            return board[0][0];
+        }
+    }
+    if( (board[0][1] == board[1][1]) && (board[0][1] == board[2][1]) ){
+        if(board[0][1] != 0){
+            debugOut("END 5");
+            return board[0][1];
+        }
+    }
+    if( (board[0][2] == board[1][2]) && (board[0][2] == board[2][2]) ){
+        if(board[0][2] != 0){
+            debugOut("END 6");
+            return board[0][2];
+        }
+    }
+    if( (board[0][0] == board[1][1]) && (board[0][0] == board[2][2]) ){
+        if(board[0][0] != 0){
+            debugOut("END 7");
+            return board[0][0];
+        }
+    }
+    if( (board[0][2] == board[1][1]) && (board[0][2] == board[2][0]) ){
+        if(board[0][2] != 0){
+            debugOut("END 8");
+            return board[0][2];
+        }
+    }
+    return 0;
 }
 void checkWin(){
     if(checkWinner() == 1){
         //X Wins
         cout << "Player X Wins!" << endl;
+        showBoard();
         gameActive = 0;
     }else if(checkWinner() == 2){
         //Y Wins
-        cout << "Player Y Wins!" << endl;
+        cout << "Player O Wins!" << endl;
+        showBoard();
         gameActive = 0;
     }else{
         debugOut("No win declared yet");
@@ -94,7 +119,7 @@ void makeMove(){
         cout << "X";
     }else if(playerTurn == 1){
         cout << "O";
-    }cout << "'s turn.";
+    }cout << "'s turn.\n";
     while(turnOver == 0){
         cout << "Select a row: ";
         cin >> row;
@@ -116,5 +141,13 @@ void debugOut(string output){
     }
 }
 
-
+char getPlayerLetter(int playerNumber){
+    if(playerNumber == 1){
+        return 'X';
+    }else if(playerNumber == 2){
+        return 'O';
+    }else{
+        return ' ';
+    }
+}
 
